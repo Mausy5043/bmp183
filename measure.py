06986832805e9773cfae444612481459d55ef9f8
Przemo Firszt <przemo@firszt.eu>
2014-08-22 21:32:04 +0100

    Cut more code
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- measure.py
+++ measure.py
@@ -1,8 +1,7 @@
 from bmp183 import bmp183
 
-if __name__ == "__main__":
-	bmp = bmp183()
-	bmp.measure_pressure()
-	print "Temperature: ", bmp.temperature, "deg C"
-	print "Pressure: ", bmp.pressure/100.0, " hPa"
-	quit()
+bmp = bmp183()
+bmp.measure_pressure()
+print "Temperature: ", bmp.temperature, "deg C"
+print "Pressure: ", bmp.pressure/100.0, " hPa"
+quit()
