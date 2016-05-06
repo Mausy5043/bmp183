a04cf524fe6e341a3a435496129fd20c585e69e6
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2016-05-06 14:09:55 +0200

    updated

--- test.py
+++ test.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 
-import sys
-import time
-import struct
-#import numpy
-
 import pigpio
 
+pi = pigpio.pi()
 
-if __name__ == "__main__":
-  pi = pigpio.pi()
-
-  if pi.connected:
+if pi.connected:
    print("Connected to pigpio daemon.")
-   hndl = pi.spi_open(0, 50000)
-   (count, rx_data) = pi.spi_xfer(hndl, b'\\xD0')
+   hndl = pi.spi_open(0, 32000)
+   (count, rx_data) = pi.spi_xfer(hndl, [0xAA])
+   # pi.spi_write(hndl, [0xAA])
+   # time.sleep(0.1)
+   # (count, rx_data) = pi.spi_read(hndl, 3)
    pi.spi_close(hndl)
 
    print count
    for i in rx_data:
-     print rx_data[i]
+       print rx_data[i]
 
-  pi.stop()
+pi.stop()
