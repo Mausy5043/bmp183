0d9c3c8c1fb9192fcecac8516c645ffc4b4ca713
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-06 13:17:42 +0200

    new update

--- update.sh
+++ update.sh
@@ -2,12 +2,14 @@
 
 branch="master"
 
-  git fetch origin
-  # Check which files have changed
-  DIFFLIST=$(git --no-pager diff --name-only "$branch..origin/$branch")
-  git pull
-  git fetch origin
-  git checkout "$branch"
-  git reset --hard "origin/$branch" && git clean -f -d
-  # Set permissions
-  chmod -R 744 ./*
+sudo rm *.pyc
+
+git fetch origin
+# Check which files have changed
+DIFFLIST=$(git --no-pager diff --name-only "$branch..origin/$branch")
+git pull
+git fetch origin
+git checkout "$branch"
+git reset --hard "origin/$branch" && git clean -f -d
+# Set permissions
+chmod -R 744 ./*
