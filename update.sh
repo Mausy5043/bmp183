7ddcfcadac0e2cd21c5bbbead0dd1998dcb7649c
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-06 13:13:24 +0200

    add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+$branch="master"
+
+  git fetch origin
+  # Check which files have changed
+  DIFFLIST=$(git --no-pager diff --name-only "$branch..origin/$branch")
+  git pull
+  git fetch origin
+  git checkout "$branch"
+  git reset --hard "origin/$branch" && git clean -f -d
+  # Set permissions
+  chmod -R 744 ./*
