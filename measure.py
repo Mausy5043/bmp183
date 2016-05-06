9b1d7aa87bd209b63a375ec7422575842524f7a3
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2016-05-06 14:41:26 +0200

    use {} and .format()

--- measure.py
+++ measure.py
@@ -2,6 +2,6 @@ from bmp183 import bmp183
 
 bmp = bmp183()
 bmp.measure_pressure()
-print ("Temperature: ", bmp.temperature, "deg C")
-print ("Pressure: ", bmp.pressure / 100.0, " hPa")
+print ("Temperature: {0} degC".format(bmp.temperature))
+print ("Pressure:    {0} hPa ".format(bmp.pressure / 100.0))
 quit()
