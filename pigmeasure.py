19e5af2aa3306aec9f5df5cb7bd57b2aad37413a
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-09 17:56:22 +0200

    migrate changes to pigbmp183

--- pigmeasure.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from pigbmp183 import bmp183
-
-bmp = bmp183()
-bmp.measure_pressure()
-print ("Temperature: {0} degC".format(bmp.temperature))
-print ("Pressure:    {0} hPa ".format(bmp.pressure / 100.0))
-quit()
